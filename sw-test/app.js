4b4254b10c2425f379ecbfebef4d94218c77bbb9
luwenli <1722072133@qq.com>
2018-02-25 17:55:08 +0800

    install fetch

--- sw-test/app.js
+++ sw-test/app.js
@@ -1,10 +1,33 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/https-demos/sw-test/sw.js', { scope: '/https-demos/sw-test/' })
+    document.querySelector("#support").innerHTML = '支持';
+    navigator.serviceWorker.register('/https-demos/sw-test/sw.js', {scope: '/https-demos/sw-test/' })
         .then(function (reg) {
-            // registration worked
-            console.log('Registration succeeded. Scope is ' + reg.scope);
+            document.querySelector("#success").innerHTML = '注册成功';
+            var serviceWorker;
+            if (reg.installing) {
+                serviceWorker = reg.installing;
+                document.querySelector("#state").innerHTML = 'installing';
+            } else if (reg.waiting) {
+                serviceWorker = reg.waiting;
+                document.querySelector("#state").innerHTML = 'waiting';
+            } else if (reg.active) {
+                serviceWorker = reg.active;
+                document.querySelector("#state").innerHTML = 'active';
+            }
+            if (serviceWorker) {
+                document.querySelector("#swState").innerHTML = serviceWorker.state;
+                serviceWorker.addEventListener('statechange', function (e) {
+                    console.log(e.target.state)
+                    document.querySelector("#swState").innerHTML += '&emsp;状态变化为' + e.target.state;
+                });
+            }
+            // reg worked
+            console.log('reg succeeded. Scope is ' + reg.scope);
         }).catch(function (error) {
-            // registration failed
-            console.log('Registration failed with ' + error);
+            // reg failed
+            document.querySelector("#success").innerHTML = '注册没有成功';            
+            console.log('reg failed with ' + error);
         });
+} else {
+    document.querySelector("#support").innerHTML = '不支持';
 }
---
+++ sw-test/sw.js
@@ -0,0 +1,40 @@
+var version = 'v6';
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(version).then(function(cache) {
+            return cache.addAll([
+                '/https-demos/sw-test/',
+                '/https-demos/sw-test/index.html',
+                '/https-demos/sw-test/style.css',
+                '/https-demos/sw-test/app.js',
+                '/https-demos/sw-test/qgw.gif',
+                '/https-demos/sw-test/offline.png'
+            ])
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+      // caches.match() always resolves
+      // but in case of success response will have value
+      if (response !== undefined) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (response) {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+          
+          caches.open(version).then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          return caches.match('/https-demos/sw-test/offline.png');
+        });
+      }
+    }));
+  });

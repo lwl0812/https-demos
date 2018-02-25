73c3c5a075077518e0b1549f87f64d91df2d8f41
luwenli <1722072133@qq.com>
2018-02-25 15:23:57 +0800

    modified the registered file url

--- sw-test/app.js
+++ sw-test/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
+    navigator.serviceWorker.register('/https-demos/sw-test/sw.js', { scope: '/https-demos/sw-test/' })
         .then(function (reg) {
             // registration worked
             console.log('Registration succeeded. Scope is ' + reg.scope);

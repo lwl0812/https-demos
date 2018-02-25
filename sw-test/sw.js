5f981a805d067f21d7218eabfb1f9bc8801c43ac
luwenli <1722072133@qq.com>
2018-02-25 19:43:20 +0800

    add activate

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -1,8 +1,8 @@
-var version = 'v6';
+var version = 'v1';
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-        caches.open(version).then(function(cache) {
+        caches.open(version).then(function (cache) {
             return cache.addAll([
                 '/https-demos/sw-test/',
                 '/https-demos/sw-test/index.html',
@@ -15,26 +15,41 @@ self.addEventListener('install', function(event) {
     );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response !== undefined) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          // response may be used only once
-          // we need to save clone to put one copy in cache
-          // and serve second one
-          let responseClone = response.clone();
-          
-          caches.open(version).then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-          return caches.match('/https-demos/sw-test/offline.png');
-        });
-      }
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    // 如果当前版本和缓存版本不一致
+                    if (cacheName !== VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(caches.match(event.request).then(function (response) {
+        // caches.match() always resolves
+        // but in case of success response will have value
+        if (response !== undefined) {
+            return response;
+        } else {
+            return fetch(event.request).then(function (response) {
+                // response may be used only once
+                // we need to save clone to put one copy in cache
+                // and serve second one
+                let responseClone = response.clone();
+
+                caches.open(version).then(function (cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(function () {
+                return caches.match('/https-demos/sw-test/offline.png');
+            });
+        }
     }));
-  });
+}); 
